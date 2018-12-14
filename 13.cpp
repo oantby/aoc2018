5711876781eacef046b22501a35e9e4f7cc35848
Logan Buth <oantby@gmail.com>
2018-12-13 21:56:54 -0600

    Finish 13-1

--- 13.cpp
+++ 13.cpp
@@ -2,13 +2,16 @@
 #include <vector>
 #include <fstream>
 #include <map>
+#include <set>
 
 using namespace std;
+
+void draw();
+void step();
 
 struct Cart {
 	uint8_t turnTicker = 0;
 	uint8_t curDir;
-	uint32_t curX, curY;
 	#define DIR_UP 1
 	#define DIR_DOWN 2
 	#define DIR_LEFT 3
@@ -32,9 +35,6 @@ struct Cart {
 		return 'I';
 	}
 	
-	void step() {
-	}
-	
 	Cart(char dir) {
 		switch (dir) {
 			case '^':
@@ -50,6 +50,21 @@ struct Cart {
 				curDir = DIR_LEFT;
 				break;
 		}
+	}
+};
+
+struct Crash {
+	string message;
+	int row, col;
+	
+	Crash(int r, int c) {
+		row = r;
+		col = c;
+		message = "Crash occurred at (X,Y) coordinate (";
+		message += to_string(col);
+		message += ',';
+		message += to_string(row);
+		message += ')';
 	}
 };
 
@@ -126,7 +141,120 @@ int main(int argc, char *argv[]) {
 		}
 		i++;
 	}
-	for (i = 0; i < 150; i++) {
+	
+	for (i = 0; i > -1; i++) {
+		try {
+			step();
+		} catch (Crash e) {
+			cout << "Crash: " << e.message << "\non iteration " << i + 1 << endl;
+			break;
+		}
+	}
+	
+	
+	return 0;
+}
+
+void step() {
+	set<Cart *> moved; // prevent from accidentally moving a cart twice.
+	const map<uint8_t, uint8_t> s_translations{
+		{DIR_UP, DIR_RIGHT},
+		{DIR_DOWN, DIR_LEFT},
+		{DIR_LEFT, DIR_DOWN},
+		{DIR_RIGHT, DIR_UP}
+	};
+	const map<uint8_t, uint8_t> b_translations{
+		{DIR_UP, DIR_LEFT},
+		{DIR_DOWN, DIR_RIGHT},
+		{DIR_LEFT, DIR_UP},
+		{DIR_RIGHT, DIR_DOWN}
+	};
+	const map<uint8_t, uint8_t> l_turn {
+		{DIR_UP, DIR_LEFT},
+		{DIR_DOWN, DIR_RIGHT},
+		{DIR_LEFT, DIR_DOWN},
+		{DIR_RIGHT, DIR_UP}
+	};
+	const map<uint8_t, uint8_t> r_turn {
+		{DIR_UP, DIR_RIGHT},
+		{DIR_DOWN, DIR_LEFT},
+		{DIR_LEFT, DIR_UP},
+		{DIR_RIGHT, DIR_DOWN}
+	};
+	for (int i = 0; i < 150; i++) {
+		for (int j = 0; j < 150; j++) {
+			if (!tracks[i].count(j)) continue;
+			if (!tracks[i][j].cart) continue;
+			Cart *c = tracks[i][j].cart;
+			
+			if (moved.count(c)) continue;
+			
+			Track *newSpot = NULL;
+			int newi, newj;
+			switch (tracks[i][j].cart->curDir) {
+				case DIR_UP:
+					newSpot = tracks[i][j].up;
+					newi = i - 1;
+					newj = j;
+					break;
+				case DIR_DOWN:
+					newSpot = tracks[i][j].down;
+					newi = i + 1;
+					newj = j;
+					break;
+				case DIR_LEFT:
+					newSpot = tracks[i][j].left;
+					newi = i;
+					newj = j - 1;
+					break;
+				case DIR_RIGHT:
+					newSpot = tracks[i][j].right;
+					newi = i;
+					newj = j + 1;
+					break;
+			}
+			if ((newSpot->cart)) {
+				throw Crash(newi, newj);
+			}
+			
+			newSpot->cart = tracks[i][j].cart;
+			tracks[i][j].cart = NULL;
+			
+			// determine cart's new direction.
+			switch (newSpot->type) {
+				case '|':
+				case '-':
+					// doesn't affect direction.
+					break;
+				case '/':
+					c->curDir = s_translations.at(c->curDir);
+					break;
+				case '\\':
+					c->curDir = b_translations.at(c->curDir);
+					break;
+				case '+':
+					switch (c->turnTicker) {
+						case 0: // turn left.
+							c->curDir = l_turn.at(c->curDir);
+							c->turnTicker++;
+							break;
+						case 1: // go straight.
+							c->turnTicker++;
+							break;
+						case 2: // turn right.
+							c->curDir = r_turn.at(c->curDir);
+							c->turnTicker = 0;
+							break;
+					}
+					break;
+			}
+			moved.insert(c);
+		}
+	}
+}
+
+void draw() {
+	for (int i = 0; i < 150; i++) {
 		for (int j = 0; j < 150; j++) {
 			if (!tracks[i].count(j)) {
 				cout << ' ';
@@ -142,5 +270,4 @@ int main(int argc, char *argv[]) {
 		cout << '\n';
 	}
 	cout << endl;
-	return 0;
 }
